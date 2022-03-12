2a1b21b0eaf8c520b5623eb56bbb15459213d706
Artur Cieslinski <jokujok1492@gmail.com>
2022-03-13 00:23:16 +0100

    6.2.-Calculator-First-Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        int result1 = calculator.add(5,10);
+        int result2 = calculator.subtract(5,10);
+
+        if (result1 == 15) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        if (result2 == -5) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    int a = 5;
+    int b = 10;
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+}
