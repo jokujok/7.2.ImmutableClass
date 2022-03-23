8ed9ed1d56903d3a26e25a5ca7ea8a75919310a3
Artur Cieslinski <jokujok1492@gmail.com>
2022-03-23 22:35:53 +0100

    6.3.ClassChoosingEvenNumbersFromCollection

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class OddNumbersExterminator {
 
-    public List<Integer> exterminate (List <Integer> numbers) {
+    public List<Integer> exterminated (List <Integer> numbers) {
         List<Integer> evens = new ArrayList<>();
 
         for (Integer number : numbers) {
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -9,52 +9,32 @@ import java.util.List;
 
 
 public class CollectionTestSuite {
-    @BeforeEach
-    public void before() {
-        System.out.println("Test Case: begin");
-    }
-
-    @AfterEach
-    public void after() {
-        System.out.println("Test Case: end");
-    }
-
-    @BeforeAll
-    public static void beforeAll() {
-        System.out.println("Test Suite: begin");
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        System.out.println("Test Suite: end");
-    }
 
     @DisplayName("given OddNumberExterminator with nothing, when calling exterminate with an empty list " +
-            "then exterminate should return empty list "
-    )
+            "then exterminate should return empty list ")
+
     @Test
     public void testOddNumbersExterminatorEmptyList() {
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         //When
         List<Integer> list = new ArrayList<Integer>();
-        List<Integer> result = oddNumbersExterminator.exterminate(list);
-        System.out.println("Testing " + result);
+        List<Integer> result = oddNumbersExterminator.exterminated(list);
         //Then
         Assertions.assertEquals(list, result);
     }
 
     @DisplayName("given OddNumberExterminator with nothing, when calling exterminate with an list" +
-            "then exterminate should return an list with even numbers "
-    )
+            "then exterminate should return an list with even numbers ")
+
     @Test
     public void testOddNumbersExterminatorNormalList() {
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         //When
-        List<Integer> startingList = Arrays.asList(1,2,3,4,5);
-        List<Integer> expectedList = Arrays.asList(2,4);
-        List<Integer> resultList = oddNumbersExterminator.exterminate(startingList);
+        List<Integer> startingList = List.of(1, 2, 3, 4, 5);
+        List<Integer> expectedList = List.of(2,4);
+        List<Integer> resultList = oddNumbersExterminator.exterminated(startingList);
         //Then
         Assertions.assertEquals(expectedList, resultList);
             }
